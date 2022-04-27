4da65497fdfc268a080bbf83bcd3e499fb6c667f
Yong Qin <yongqin3759@gmail.com>
2022-04-28 01:27:44 +0800

    update

--- notes-app/backend/utils/configs.js
+++ notes-app/backend/utils/configs.js
@@ -6,6 +6,10 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
+
+console.log(PORT)
+console.log(MONGODB_URI)
+
 module.exports = {
   MONGODB_URI,
   PORT
