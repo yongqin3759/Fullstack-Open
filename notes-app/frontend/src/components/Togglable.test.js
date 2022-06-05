108371f38822c522351c040d76349da87f9e568e
Foo Yong Qin <yongqinfoo@gmail.com>
2022-06-05 14:17:46 +0800

    updated test

--- notes-app/frontend/src/components/Togglable.test.js
+++ notes-app/frontend/src/components/Togglable.test.js
@@ -4,7 +4,7 @@ import {render, screen} from '@testing-library/react'
 import userEvent  from "@testing-library/user-event";
 import Togglable from './Togglable'
 
-describe('<Togglable', ()=> {
+describe('<Togglable/>', ()=> {
     let container
     beforeEach(()=> {
         container = render(
