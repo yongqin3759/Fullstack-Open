98dfb877d403f400dd825c5125af5474509334fc
Foo Yong Qin <yongqin3759@gmail.com>
2022-06-07 02:24:57 +0800

    e2e testing

--- notes-app/frontend/cypress/e2e/ note_app.spec.js
+++ notes-app/frontend/cypress/e2e/ note_app.spec.js
@@ -1,6 +1,28 @@
 describe('Note app', function() {
   beforeEach(function(){
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Yong Qin',
+      username: 'qin',
+      password: 'yongqin'
+    }
+
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
+  })
+
+
+  it('login fails with wrong password', function(){
+    cy.contains('login').click()
+    cy.get('input:first').type('qin')
+    cy.get('input:last').type('jeineonfo')
+    cy.get('#login-button').click()
+
+    cy.get('.error').should('contain', 'Wrong credentials')
+                    .and('have.css', 'color', 'rgb(255, 0, 0)')
+                    .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Yong Qin logged in')
   })
 
   it('front page can be opened', function() {
@@ -13,10 +35,44 @@ describe('Note app', function() {
   })
   it('user can login', function () {
     cy.contains('login').click()
-    cy.get('input:first').type('mluukkai')
-    cy.get('input:last').type('salainen')
+    cy.get('input:first').type('qin')
+    cy.get('input:last').type('yongqin')
     cy.get('#login-button').click()
 
-    cy.contains('Matti Luukkainen logged in')
-  })  
+    cy.contains('Yong Qin logged in')
+  }) 
+  
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('input:first').type('qin')
+      cy.get('input:last').type('yongqin')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exists', function(){
+      beforeEach(function(){
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+
+      it('it can be made important', function(){
+        cy.contains('another note cypress')
+          .contains('make important')
+          .click()
+        
+        cy.contains('another note cypress')
+          .contains('make not important')
+      })
+    })
+  })
 })
